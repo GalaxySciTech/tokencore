3b8eba3f6f12be4f8e3e2130151360a29f0a1826
pie <429224280@qq.com>
2019-03-05 18:35:15 +0800

    update

--- src/main/java/org/consenlabs/tokencore/wallet/transaction/EOSTransaction.java
+++ src/main/java/org/consenlabs/tokencore/wallet/transaction/EOSTransaction.java
@@ -1,5 +1,7 @@
 package org.consenlabs.tokencore.wallet.transaction;
 
+import io.eblock.eos4j.OfflineSign;
+import io.eblock.eos4j.api.vo.SignParam;
 import io.eblock.eos4j.ese.Action;
 import io.eblock.eos4j.ese.DataParam;
 import io.eblock.eos4j.ese.DataType;
@@ -8,7 +10,9 @@ import org.consenlabs.tokencore.foundation.crypto.Hash;
 import org.consenlabs.tokencore.foundation.utils.ByteUtil;
 import org.consenlabs.tokencore.foundation.utils.NumericUtil;
 import org.consenlabs.tokencore.wallet.Wallet;
+import org.consenlabs.tokencore.wallet.WalletManager;
 import org.consenlabs.tokencore.wallet.keystore.V3Keystore;
+import org.consenlabs.tokencore.wallet.model.TokenException;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -16,97 +20,109 @@ import java.util.List;
 
 public class EOSTransaction implements TransactionSigner {
 
-  private byte[] txBuf;
-  private List<ToSignObj> txsToSign;
+    private byte[] txBuf;
+    private List<ToSignObj> txsToSign;
+    private String from;
+    private String to;
+    private String quantity;
+    private String memo;
+    private SignParam signParam;
 
-  public EOSTransaction(byte[] txBuf) {
-    this.txBuf = txBuf;
-  }
-
-  public EOSTransaction(List<ToSignObj> txsToSign) {
-    this.txsToSign = txsToSign;
-  }
-
-  public EOSTransaction(String from, String to, String quantity, String memo){
-    DataParam[] datas = new DataParam[] { new DataParam(from, DataType.name, Action.transfer),
-            new DataParam(to, DataType.name, Action.transfer),
-            new DataParam(quantity, DataType.asset, Action.transfer),
-            new DataParam(memo, DataType.string, Action.transfer), };
-    byte[] allbyte = new byte[] {};
-    for (DataParam value : datas) {
-      allbyte = ByteUtils.concat(allbyte, value.seria());
+    public EOSTransaction(byte[] txBuf) {
+        this.txBuf = txBuf;
     }
-    this.txBuf=allbyte;
-  }
 
+    public EOSTransaction(List<ToSignObj> txsToSign) {
+        this.txsToSign = txsToSign;
+    }
 
-  @Deprecated
-  @Override
-  public TxSignResult signTransaction(String chainId, String password, Wallet wallet) {
-    String transactionID = NumericUtil.bytesToHex(Hash.sha256(txBuf));
-    txBuf = ByteUtil.concat(NumericUtil.hexToBytes(chainId), txBuf);
+    public EOSTransaction(SignParam signParam, String from, String to, String quantity, String memo) {
+        this.from = from;
+        this.to = to;
+        this.quantity = quantity;
+        this.memo = memo;
+        this.signParam = signParam;
+    }
 
-    byte[] zeroBuf = new byte[32];
-    Arrays.fill(zeroBuf, (byte) 0);
-    txBuf = ByteUtil.concat(txBuf, zeroBuf);
+    public TxSignResult signTransaction(String password, Wallet wallet, String pubKey) {
+        String wif = NumericUtil.bytesToHex(wallet.decryptPrvKeyFor(pubKey, password));
+        OfflineSign sign = new OfflineSign();
+        try {
+            String content = sign.transfer(signParam, wif, "eosio.token",
+                    from, to, quantity, "");
+            return new TxSignResult(content, "");
+        } catch (Exception e) {
+            throw new TokenException(e.getMessage(), e);
+        }
+    }
 
-    String wif = wallet.exportPrivateKey(password);
-    String signed = EOSSign.sign(Hash.sha256(txBuf), wif);
+    @Deprecated
+    @Override
+    public TxSignResult signTransaction(String chainId, String password, Wallet wallet) {
+        String transactionID = NumericUtil.bytesToHex(Hash.sha256(txBuf));
+        txBuf = ByteUtil.concat(NumericUtil.hexToBytes(chainId), txBuf);
 
-    return new TxSignResult(signed, transactionID);
-  }
+        byte[] zeroBuf = new byte[32];
+        Arrays.fill(zeroBuf, (byte) 0);
+        txBuf = ByteUtil.concat(txBuf, zeroBuf);
 
-  public List<TxMultiSignResult> signTransactions(String chainId, String password, Wallet wallet) {
-    List<TxMultiSignResult> results = new ArrayList<>(txsToSign.size());
-    for (ToSignObj toSignObj : txsToSign) {
+        String wif = wallet.exportPrivateKey(password);
+        String signed = EOSSign.sign(Hash.sha256(txBuf), wif);
 
-      byte[] txBuf = NumericUtil.hexToBytes(toSignObj.txHex);
-      String transactionID = NumericUtil.bytesToHex(Hash.sha256(txBuf));
+        return new TxSignResult(signed, transactionID);
+    }
 
-      byte[] txChainIDBuf = ByteUtil.concat(NumericUtil.hexToBytes(chainId), txBuf);
+    public List<TxMultiSignResult> signTransactions(String chainId, String password, Wallet wallet) {
+        List<TxMultiSignResult> results = new ArrayList<>(txsToSign.size());
+        for (ToSignObj toSignObj : txsToSign) {
 
-      byte[] zeroBuf = new byte[32];
-      Arrays.fill(zeroBuf, (byte) 0);
-      byte[] fullTxBuf = ByteUtil.concat(txChainIDBuf, zeroBuf);
+            byte[] txBuf = NumericUtil.hexToBytes(toSignObj.txHex);
+            String transactionID = NumericUtil.bytesToHex(Hash.sha256(txBuf));
 
-      byte[] hashedTx = Hash.sha256(fullTxBuf);
+            byte[] txChainIDBuf = ByteUtil.concat(NumericUtil.hexToBytes(chainId), txBuf);
 
-      List<String> signatures = new ArrayList<>(toSignObj.publicKeys.size());
-      for (String pubKey : toSignObj.publicKeys) {
-        String signed;
-        if (wallet.getKeystore().getVersion() == V3Keystore.VERSION) {
-          signed = EOSSign.sign(hashedTx, wallet.exportPrivateKey(password));
-        } else {
-          signed = EOSSign.sign(hashedTx, wallet.decryptPrvKeyFor(pubKey, password));
+            byte[] zeroBuf = new byte[32];
+            Arrays.fill(zeroBuf, (byte) 0);
+            byte[] fullTxBuf = ByteUtil.concat(txChainIDBuf, zeroBuf);
+
+            byte[] hashedTx = Hash.sha256(fullTxBuf);
+
+            List<String> signatures = new ArrayList<>(toSignObj.publicKeys.size());
+            for (String pubKey : toSignObj.publicKeys) {
+                String signed;
+                if (wallet.getKeystore().getVersion() == V3Keystore.VERSION) {
+                    signed = EOSSign.sign(hashedTx, wallet.exportPrivateKey(password));
+                } else {
+                    signed = EOSSign.sign(hashedTx, wallet.decryptPrvKeyFor(pubKey, password));
+                }
+
+                signatures.add(signed);
+            }
+
+            TxMultiSignResult signedResult = new TxMultiSignResult(transactionID, signatures);
+            results.add(signedResult);
+        }
+        return results;
+    }
+
+    public static class ToSignObj {
+        private String txHex;
+        private List<String> publicKeys;
+
+        public String getTxHex() {
+            return txHex;
         }
 
-        signatures.add(signed);
-      }
+        public void setTxHex(String txHex) {
+            this.txHex = txHex;
+        }
 
-      TxMultiSignResult signedResult = new TxMultiSignResult(transactionID, signatures);
-      results.add(signedResult);
+        public List<String> getPublicKeys() {
+            return publicKeys;
+        }
+
+        public void setPublicKeys(List<String> publicKeys) {
+            this.publicKeys = publicKeys;
+        }
     }
-    return results;
-  }
-
-  public static class ToSignObj {
-    private String txHex;
-    private List<String> publicKeys;
-
-    public String getTxHex() {
-      return txHex;
-    }
-
-    public void setTxHex(String txHex) {
-      this.txHex = txHex;
-    }
-
-    public List<String> getPublicKeys() {
-      return publicKeys;
-    }
-
-    public void setPublicKeys(List<String> publicKeys) {
-      this.publicKeys = publicKeys;
-    }
-  }
 }
