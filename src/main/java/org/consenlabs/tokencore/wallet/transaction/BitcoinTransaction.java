5448ce446d6f5219e4fc53ed8b5ae4046e92927f
pie <429224280@qq.com>
2020-12-10 18:07:40 +0800

    添加omni usdt隔离见证转账 有问题即时反映我

--- src/main/java/org/consenlabs/tokencore/wallet/transaction/BitcoinTransaction.java
+++ src/main/java/org/consenlabs/tokencore/wallet/transaction/BitcoinTransaction.java
@@ -455,6 +455,189 @@ public class BitcoinTransaction implements TransactionSigner {
         return new TxSignResult(signedHex, txHash);
     }
 
+    public TxSignResult signUsdtSegWitTransaction(String chainId, String password, Wallet wallet) {
+        collectPrvKeysAndAddress(Metadata.P2WPKH, password, wallet);
+
+        long totalAmount = 0L;
+        boolean hasChange = false;
+
+        for (UTXO output : getOutputs()) {
+            totalAmount += output.getAmount();
+        }
+
+        if (totalAmount < getAmount()) {
+            throw new TokenException(Messages.INSUFFICIENT_FUNDS);
+        }
+
+        long changeAmount = totalAmount - (getAmount() + getFee());
+        Address toAddress = Address.fromBase58(network, to);
+        byte[] targetScriptPubKey;
+        if (toAddress.isP2SHAddress()) {
+            targetScriptPubKey = ScriptBuilder.createP2SHOutputScript(toAddress.getHash160()).getProgram();
+        } else {
+            targetScriptPubKey = ScriptBuilder.createOutputScript(toAddress).getProgram();
+        }
+
+        byte[] changeScriptPubKey = ScriptBuilder.createP2SHOutputScript(changeAddress.getHash160()).getProgram();
+
+        byte[] hashPrevouts;
+        byte[] hashOutputs;
+        byte[] hashSequence;
+
+        try {
+            // calc hash prevouts
+            UnsafeByteArrayOutputStream stream = new UnsafeByteArrayOutputStream();
+            for (UTXO utxo : getOutputs()) {
+                TransactionOutPoint outPoint = new TransactionOutPoint(this.network, utxo.vout, Sha256Hash.wrap(utxo.txHash));
+                outPoint.bitcoinSerialize(stream);
+            }
+            hashPrevouts = Sha256Hash.hashTwice(stream.toByteArray());
+
+            // calc hash outputs
+            stream = new UnsafeByteArrayOutputStream();
+
+            String usdtHex = "6a146f6d6e69" + String.format("%016x", 31) + String.format("%016x", amount);
+            TransactionOutput usdtOutput = new TransactionOutput(this.network, null, Coin.valueOf(0L), new Script(Utils.HEX.decode(usdtHex)).getProgram());
+            usdtOutput.bitcoinSerialize(stream);
+
+            TransactionOutput targetOutput = new TransactionOutput(this.network, null, Coin.valueOf(amount), toAddress);
+            targetOutput.bitcoinSerialize(stream);
+
+            if (changeAmount >= DUST_THRESHOLD) {
+                hasChange = true;
+                TransactionOutput changeOutput = new TransactionOutput(this.network, null, Coin.valueOf(changeAmount), changeAddress);
+                changeOutput.bitcoinSerialize(stream);
+            }
+
+//
+//      Utils.uint64ToByteStreamLE(BigInteger.valueOf(amount), stream);
+//      stream.write(new VarInt(targetScriptPubKey.length).encode());
+//      stream.write(targetScriptPubKey);
+//      Utils.uint64ToByteStreamLE(BigInteger.valueOf(changeAmount), stream);
+//      stream.write(new VarInt(changeScriptPubKey.length).encode());
+//      stream.write(changeScriptPubKey);
+
+            hashOutputs = Sha256Hash.hashTwice(stream.toByteArray());
+
+            // calc hash sequence
+            stream = new UnsafeByteArrayOutputStream();
+
+            for (UTXO utxo : getOutputs()) {
+                Utils.uint32ToByteStreamLE(utxo.getSequence(), stream);
+            }
+            hashSequence = Sha256Hash.hashTwice(stream.toByteArray());
+
+            // calc witnesses and redemScripts
+            List<byte[]> witnesses = new ArrayList<>();
+            List<String> redeemScripts = new ArrayList<>();
+            for (int i = 0; i < getOutputs().size(); i++) {
+                UTXO utxo = getOutputs().get(i);
+                BigInteger prvKey = Metadata.FROM_WIF.equals(wallet.getMetadata().getSource()) ? prvKeys.get(0) : prvKeys.get(i);
+                ECKey key = ECKey.fromPrivate(prvKey, true);
+                String redeemScript = String.format("0014%s", NumericUtil.bytesToHex(key.getPubKeyHash()));
+                redeemScripts.add(redeemScript);
+
+                // calc outpoint
+                stream = new UnsafeByteArrayOutputStream();
+                TransactionOutPoint txOutPoint = new TransactionOutPoint(this.network, utxo.vout, Sha256Hash.wrap(utxo.txHash));
+                txOutPoint.bitcoinSerialize(stream);
+                byte[] outpoint = stream.toByteArray();
+
+                // calc scriptCode
+                byte[] scriptCode = NumericUtil.hexToBytes(String.format("0x1976a914%s88ac", NumericUtil.bytesToHex(key.getPubKeyHash())));
+
+                // before sign
+                stream = new UnsafeByteArrayOutputStream();
+                Utils.uint32ToByteStreamLE(2L, stream);
+                stream.write(hashPrevouts);
+                stream.write(hashSequence);
+                stream.write(outpoint);
+                stream.write(scriptCode);
+                Utils.uint64ToByteStreamLE(BigInteger.valueOf(utxo.getAmount()), stream);
+                Utils.uint32ToByteStreamLE(utxo.getSequence(), stream);
+                stream.write(hashOutputs);
+                Utils.uint32ToByteStreamLE(locktime, stream);
+                // hashType 1 = all
+                Utils.uint32ToByteStreamLE(1L, stream);
+
+                byte[] hashPreimage = stream.toByteArray();
+                byte[] sigHash = Sha256Hash.hashTwice(hashPreimage);
+                ECKey.ECDSASignature signature = key.sign(Sha256Hash.wrap(sigHash));
+                byte hashType = 0x01;
+                // witnesses
+                byte[] sig = ByteUtil.concat(signature.encodeToDER(), new byte[]{hashType});
+                witnesses.add(sig);
+            }
+
+
+            // the second stream is used to calc the traditional txhash
+            UnsafeByteArrayOutputStream[] serialStreams = new UnsafeByteArrayOutputStream[]{
+                    new UnsafeByteArrayOutputStream(), new UnsafeByteArrayOutputStream()
+            };
+            for (int idx = 0; idx < 2; idx++) {
+                stream = serialStreams[idx];
+                Utils.uint32ToByteStreamLE(2L, stream); // version
+                if (idx == 0) {
+                    stream.write(0x00); // maker
+                    stream.write(0x01); // flag
+                }
+                // inputs
+                stream.write(new VarInt(getOutputs().size()).encode());
+                for (int i = 0; i < getOutputs().size(); i++) {
+                    UTXO utxo = getOutputs().get(i);
+                    stream.write(NumericUtil.reverseBytes(NumericUtil.hexToBytes(utxo.txHash)));
+                    Utils.uint32ToByteStreamLE(utxo.getVout(), stream);
+
+                    // the length of byte array that follows, and this length is used by OP_PUSHDATA1
+                    stream.write(0x17);
+                    // the length of byte array that follows, and this length is used by cutting array
+                    stream.write(0x16);
+                    stream.write(NumericUtil.hexToBytes(redeemScripts.get(i)));
+                    Utils.uint32ToByteStreamLE(utxo.getSequence(), stream);
+                }
+
+                // outputs
+                // outputs size
+                int outputSize = hasChange ? 2 : 1;
+                stream.write(new VarInt(outputSize).encode());
+                Utils.uint64ToByteStreamLE(BigInteger.valueOf(amount), stream);
+                stream.write(new VarInt(targetScriptPubKey.length).encode());
+                stream.write(targetScriptPubKey);
+                if (hasChange) {
+                    Utils.uint64ToByteStreamLE(BigInteger.valueOf(changeAmount), stream);
+                    stream.write(new VarInt(changeScriptPubKey.length).encode());
+                    stream.write(changeScriptPubKey);
+                }
+
+                // the first stream is used to calc the segwit hash
+                if (idx == 0) {
+                    for (int i = 0; i < witnesses.size(); i++) {
+                        BigInteger prvKey = Metadata.FROM_WIF.equals(wallet.getMetadata().getSource()) ? prvKeys.get(0) : prvKeys.get(i);
+
+                        ECKey ecKey = ECKey.fromPrivate(prvKey);
+                        byte[] wit = witnesses.get(i);
+                        stream.write(new VarInt(2).encode());
+                        stream.write(new VarInt(wit.length).encode());
+                        stream.write(wit);
+                        stream.write(new VarInt(ecKey.getPubKey().length).encode());
+                        stream.write(ecKey.getPubKey());
+                    }
+                }
+
+                Utils.uint32ToByteStreamLE(locktime, stream);
+            }
+            byte[] signed = serialStreams[0].toByteArray();
+            String signedHex = NumericUtil.bytesToHex(signed);
+            String wtxID = NumericUtil.bytesToHex(Sha256Hash.hashTwice(signed));
+            wtxID = NumericUtil.beBigEndianHex(wtxID);
+            String txHash = NumericUtil.bytesToHex(Sha256Hash.hashTwice(serialStreams[1].toByteArray()));
+            txHash = NumericUtil.beBigEndianHex(txHash);
+            return new TxSignResult(signedHex, txHash, wtxID);
+        } catch (IOException ex) {
+            throw new TokenException("OutputStream error");
+        }
+    }
+
     public TxSignResult signSegWitTransaction(String chainId, String password, Wallet wallet) {
         collectPrvKeysAndAddress(Metadata.P2WPKH, password, wallet);
 
